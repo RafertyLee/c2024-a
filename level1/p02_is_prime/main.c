712a34f4c9aab33a8baea82cd7fc4bd4fa0cffc6
Raferty Lee <Raferty.OpenSource@outlook.com>
2024-09-21 12:09:38 +0800

    Level1 p02 Finished

--- level1/p02_is_prime/main.c
+++ level1/p02_is_prime/main.c
@@ -1,6 +1,25 @@
+#include <math.h>
 #include <stdio.h>
 
 int main() {
-    printf("hello world!\n");
+    int number;
+    printf("Enter a number: ");
+    scanf("%d", &number);
+    int limit = sqrt(number);
+    if (number >= 2) {
+        for (int i = limit; i < number; i++) {
+            if (number % i == 0) {
+                printf("%d is not prime\n", i);
+                return 0;
+            }
+            else if (i == number - 1) {
+                printf("%d is prime\n", number);
+            }
+        }
+    } else {
+        printf("%d <=1 \n", number);
+    }
     return 0;
+
+
 }
