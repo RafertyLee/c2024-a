359dcd1f136002c073b9c4b774e35efdcbbed8db
Raferty Lee <Raferty.OpenSource@outlook.com>
2024-09-21 12:42:13 +0800

    Level1 p03 Finished

--- level1/p03_all_primes/main.c
+++ level1/p03_all_primes/main.c
@@ -1,6 +1,24 @@
+#include <math.h>
 #include <stdio.h>
 
 int main() {
-    printf("hello world!\n");
+    int prime[500];
+    int count = 1;
+    prime[0] = 3;
+    for (int i = 5; i <= 1000; i += 2) {
+        for (int j = count - 1; ; j -= 1) {
+            if (i % prime[j] == 0) {
+                break;
+            } else if(j == 0) {
+                printf("%d\n", i);
+                prime[count] = i;
+                count++;
+
+                break;
+            }
+        }
+
+
+    }
     return 0;
 }
