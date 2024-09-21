52fac122b4a78cff3aca5859d769726658ab04bf
RafertyLee <Raferty.OpenSource@outlook.com>
2024-09-21 11:18:36 +0800

    Level1 p01 Finished

--- level1/p01_running_letter/main.c
+++ level1/p01_running_letter/main.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
+#include <ncursesw/ncurses.h>
+#include <stdlib.h>
 
 int main() {
-    printf("hello world!\n");
+    initscr();            // 初始化屏幕
+    int n = 0;
+    int v = 1;
+
+
+
+    while(1) {
+
+        curs_set(FALSE);
+        mvprintw( 0,n,"Hello World");
+        refresh();
+        n += v;
+        if (n > COLS-11){
+            v = -1;
+        }
+        else if (n < 0) {
+            v = 1;
+        }
+        _sleep(100);
+        clear();
+
+    }
+
+    refresh();
+
+    endwin();
     return 0;
 }
