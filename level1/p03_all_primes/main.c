d66d2909f6a5e6fc07d004f03438bebf4d2208be
RafertyLee <Raferty.Opensource@outlook.com>
2024-09-23 18:49:34 +0800

    Level1 p04 Finished & Prime Number Corrected

--- level1/p03_all_primes/main.c
+++ level1/p03_all_primes/main.c
@@ -3,14 +3,14 @@
 
 int main() {
     int prime[500];
-    int count = 1;
-    prime[0] = 3;
+    int count = 2;
+    prime[0] = 2;
+    prime[1] = 3;
     for (int i = 5; i <= 1000; i += 2) {
         for (int j = count - 1; ; j -= 1) {
             if (i % prime[j] == 0) {
                 break;
             } else if(j == 0) {
-                printf("%d\n", i);
                 prime[count] = i;
                 count++;
 
@@ -19,6 +19,9 @@ int main() {
         }
 
 
+    }
+    for (int i = 0; i < count; i++) {
+        printf("%d\n", prime[i]);
     }
     return 0;
 }
