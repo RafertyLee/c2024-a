a3a4d4ee757621640e6f31eb66250729cafac491
RafertyLee <Raferty.Opensource@outlook.com>
2024-09-25 01:31:05 +0800

    Level1 p03 Time Count Added (Cross Platform) & p01 Improved

--- level1/p01_running_letter/main.c
+++ level1/p01_running_letter/main.c
@@ -2,28 +2,26 @@
 #include <ncursesw/ncurses.h>
 #include <stdlib.h>
 
+const unsigned long INTERVAL = 100;
+
 int main() {
-    initscr();            // 初始化屏幕
+    initscr();
     int n = 0;
     int v = 1;
 
 
-
-    while(1) {
-
+    while (1) {
         curs_set(FALSE);
-        mvprintw( 0,n,"Hello World");
+        mvprintw(0, n, "Hello World");
         refresh();
         n += v;
-        if (n > COLS-11){
+        if (n > COLS - 11) {
             v = -1;
-        }
-        else if (n < 0) {
+        } else if (n < 0) {
             v = 1;
         }
-        _sleep(100);
+        _sleep(INTERVAL);
         clear();
-
     }
 
     refresh();
