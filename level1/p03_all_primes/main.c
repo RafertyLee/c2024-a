225c01ef90875b48d1cbfbe63518508b760ce58f
Raferty Lee <Raferty.Opensource@outlook.com>
2024-09-25 01:31:05 +0800

    p03 Time Count Added (Cross Platform) & p01 Improved

--- level1/p03_all_primes/main.c
+++ level1/p03_all_primes/main.c
@@ -1,8 +1,34 @@
-#include <math.h>
 #include <stdio.h>
+#include <time.h>
+
+#ifdef _WIN32
+#include <windows.h>
+unsigned long long get_current_time_in_microseconds() {
+    LARGE_INTEGER frequency, currentTime;
+    QueryPerformanceFrequency(&frequency);
+    QueryPerformanceCounter(&currentTime);
+
+
+    return (currentTime.QuadPart * 1000000) / frequency.QuadPart;
+}
+
+
+#else
+#include <sys/time.h>]
+long long get_current_time_in_microseconds() {
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return (tv.tv_sec * 1000000LL) + tv.tv_usec;
+}
+#endif
 
 int main() {
+    unsigned long long start_time = get_current_time_in_microseconds();
+
     int prime[500];
+    for (int i = 0; i < 500; i++) {
+        prime[i] = 0;
+    }
     int count = 2;
     prime[0] = 2;
     prime[1] = 3;
@@ -20,8 +46,14 @@ int main() {
 
 
     }
+    clock_t end = clock ();
     for (int i = 0; i < count; i++) {
+        if (prime[i] == 0) {
+            break;
+        }
         printf("%d\n", prime[i]);
     }
+    unsigned long long end_time = get_current_time_in_microseconds();
+    printf("time = %lld us",end_time-start_time);
     return 0;
 }
